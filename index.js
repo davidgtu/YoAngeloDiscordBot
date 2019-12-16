e34470b5f93edd3485756100201ba8c8e47fe0d2
davidgtu <david.giahuy.tu@gmail.com>
2019-12-16 09:21:34 -0500

    add project

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const Discord = require('discord.js'); 
+const client = new Discord.Client();
+
+client.login(process.env.CLIENT_ID);
+
+client.on('ready', () => {   
+    console.log('🗿 JosukeBot Ready 🗿'); 
+});
+
+let yoCount = 0
+
+const resetYoCount = () => yoCount = 0;
+const logger = (logMessage) => console.log(`🗿 ${logMessage} 🗿`)
+
+client.on('message', (msg) => {
+    if (msg.content.includes('🗿')) {
+        logger(`Replying to author ${msg.author.username} at ${new Date().toLocaleTimeString()}`)
+        if (yoCount === 5) {
+            msg.channel.send({
+                files: ['./image/koichiyoangelo.jpg']
+            });
+            resetYoCount();
+        } else {
+            msg.channel.send({
+                files: ['./image/yoangelo.jpg']
+            });
+        }
+
+        yoCount++;
+        logger(`yoCount is ${yoCount}`);
+
+    };
+});
