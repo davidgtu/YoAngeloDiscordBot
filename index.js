cfcb4b8b0b4c6077ebc42c11fe8bebab78611d5f
davidgtu <david.giahuy.tu@gmail.com>
2019-12-16 12:51:51 -0500

    add error handling

--- index.js
+++ index.js
@@ -14,22 +14,25 @@ let yoCount = 0
 const resetYoCount = () => yoCount = 0;
 const logger = (logMessage) => console.log(`🗿 ${logMessage} 🗿`)
 
-client.on('message', (msg) => {
-    if (msg.content.includes('🗿')) {
-        logger(`Replying to author ${msg.author.username} at ${new Date().toLocaleTimeString()}`)
-        if (yoCount === 5) {
-            msg.channel.send({
-                files: ['./image/koichiyoangelo.jpg']
-            });
-            resetYoCount();
-        } else {
-            msg.channel.send({
-                files: ['./image/yoangelo.gif']
-            });
-        }
-
-        yoCount++;
-        logger(`yoCount is ${yoCount}`);
-
-    };
-});
+try {
+    client.on('message', (msg) => {
+        if (msg.content.includes('🗿')) {
+            logger(`Replying to author ${msg.author.username} at ${new Date().toLocaleTimeString()}`)
+            if (yoCount === 5) {
+                msg.channel.send({
+                    files: ['./image/koichiyoangelo.jpg']
+                });
+                resetYoCount();
+            } else {
+                msg.channel.send({
+                    files: ['./image/yoangelo.gif']
+                });
+            }
+    
+            yoCount++;
+            logger(`yoCount is ${yoCount}`);
+        };
+    });
+} catch (error) {
+    console.error(error)
+}
